227f34cb0fba917f48d8de742fea263c89fd374a
Hanna Alea <your_email@example.com>
2025-11-28 17:06:56 +0100

    Commit 3: Updated UserInputValidatorInstances Class

--- UserInputValidatorInstances.py
+++ UserInputValidatorInstances.py
@@ -7,3 +7,10 @@ validator2 = UserInputValidator()
 # Sample input lists
 inputs1 = ["5", "25", "-3", "uigh", "10"]
 inputs2 = ["9", ".5", "0", "*Annyeong", "-9"]
+
+result1 = validator1.validate_positive_integers(inputs1)
+result2 = validator2.validate_positive_integers(inputs2)
+
+# Print the results
+print("Validator 1 valid integers:", result1)
+print("Validator 2 valid integers:", result2)
