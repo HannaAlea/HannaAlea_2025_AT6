2c8e501f8a1df6834909aacbe5b23fb7f41f7cfe
Hanna Alea <your_email@example.com>
2025-11-28 17:02:54 +0100

    Commit 2: added UserInputValidatorInstances class, .gitignore and extra.txt

---
+++ UserInputValidatorInstances.py
@@ -0,0 +1,9 @@
+from UserInputValidator import UserInputValidator
+
+
+validator1 = UserInputValidator()
+validator2 = UserInputValidator()
+
+# Sample input lists
+inputs1 = ["5", "25", "-3", "uigh", "10"]
+inputs2 = ["9", ".5", "0", "*Annyeong", "-9"]
